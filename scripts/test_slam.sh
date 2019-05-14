16de275f28680fc63df83cba7f75aa1c56483336
Kevin <kevmohsu@gmail.com>
2019-05-14 18:32:36 +0000

    Added test_slam.sh

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-catkin_dir=/home/workspace/catkin_ws
+catkin_dir=/home/workspace/home_service_bot/catkin_ws
 catkin_src_dir=$catkin_dir/src
 
 #Launch turtlebot in the custom world
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
-sleep 2
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$catkin_src_dir/map/apartment.world" &
+sleep 5
 
 #Launch gmapping demo
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 2
 
 #Launch turtlebot teleop
 xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch" &
